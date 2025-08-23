37ce2475827b3fad23001053a16b5b37d0bff988
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-08-23 23:05:17 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,17 +6,7 @@ sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
 
 ARCH="$(uname -m)"
 
-if [ "$ARCH" = 'x86_64' ]; then
-	PKG_TYPE="$ARCH.pkg.tar.zst"
-elif [ "$ARCH" = 'aarch64' ]; then
-	PKG_TYPE="$ARCH.pkg.tar.xz"
-fi
-
-LIBXML2_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/libxml2-mini-$PKG_TYPE"
-OPUS_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/opus-mini-$PKG_TYPE"
-MESA_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/mesa-nano-$PKG_TYPE"
-INTEL_MEDIA_DRIVER_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/intel-media-driver-mini-$PKG_TYPE" 
-GTK4_URL="https://github.com/pkgforge-dev/archlinux-pkgs-debloated/releases/download/continuous/gtk4-mini-$PKG_TYPE"
+DEBLOATED_PKGS_INSTALLER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
 echo "Installing build dependencies for sharun & AppImage integration..."
 echo "---------------------------------------------------------------"
@@ -36,13 +26,9 @@ pacman -Syu --noconfirm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$LIBXML2_URL"  -O  ./"libxml2-mini-$PKG_TYPE"
-wget --retry-connrefused --tries=30 "$OPUS_URL"    -O  ./"opus-mini-$PKG_TYPE"
-wget --retry-connrefused --tries=30 "$MESA_URL"        -O  ./"mesa-nano-$PKG_TYPE"
-wget --retry-connrefused --tries=30 "$GTK4_URL"        -O  ./"gtk4-mini-$PKG_TYPE"
-if [ "$ARCH" = 'x86_64' ]; then
-  wget --retry-connrefused --tries=30 "$INTEL_MEDIA_DRIVER_URL"  -O ./"intel-media-driver-mini-$PKG_TYPE"
-fi
+wget --retry-connrefused --tries=30 "$DEBLOATED_PKGS_INSTALLER" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh libxml2-mini opus-mini mesa-nano intel-media-driver-mini gtk4-mini
 
 pacman -U --noconfirm ./*.pkg.*
 rm -f ./*.pkg.*
