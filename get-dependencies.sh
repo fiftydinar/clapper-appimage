55b8f1d7aa2ebd79ec0170c8a6603e5fee65ac2d
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-08-23 23:06:38 +0200

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -30,8 +30,5 @@ wget --retry-connrefused --tries=30 "$DEBLOATED_PKGS_INSTALLER" -O ./get-debloat
 chmod +x ./get-debloated-pkgs.sh
 ./get-debloated-pkgs.sh libxml2-mini opus-mini mesa-nano intel-media-driver-mini gtk4-mini
 
-pacman -U --noconfirm ./*.pkg.*
-rm -f ./*.pkg.*
-
 echo "All done!"
 echo "---------------------------------------------------------------"
